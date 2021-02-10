024b36857cd781d44628ee3021bd91268b6bd3e7
alailsonko <aalailson3@gmail.com>
2021-02-10 11:08:20 -0300

    feat: change the strategy

--- erlangblog/src/signup_controller.erl
+++ erlangblog/src/signup_controller.erl
@@ -8,7 +8,7 @@ json_to_map(Data) -> jsx:decode(Data).
 username_validation(Username) -> Username,
     if 
         Username == <<>> -> 
-            <<"{\"error\":\"username is required\"}">>;
+            {false, <<"{\"error\":\"username is required\"}">>};
         true -> 
             true
     end.
@@ -18,7 +18,21 @@ email_validation(Email) -> Email,
             <<"{\"error\":\"email is required\"}">>;
         true -> 
             true
-    end.    
+    end.
+password_validation(Password) -> Password,
+    if 
+        Password == <<>> ->
+            <<"{\"error\":\"password is required\"}">>;
+        true -> 
+            true
+    end.   
+passwordConfirm_validation(PasswordConfirm) -> PasswordConfirm,
+    if 
+        PasswordConfirm == <<>> ->
+            <<"{\"error\":\"passwordConfirm is required\"}">>;
+        true -> 
+            true
+    end.
 
 init(Req0, State) ->
     {ok, Data, Req1} = cowboy_req:read_body(Req0),
@@ -33,11 +47,20 @@ init(Req0, State) ->
     io:format("~p.~n", [Email]),
     io:format("~p.~n", [email_validation(Email)]),
     io:format("~p.~n", [Password]),
-    % io:format("~p.~n", [username_validation(Username)]),
+    io:format("~p.~n", [password_validation(Password)]),
     io:format("~p.~n", [PasswordConfirm]),
-    % io:format("~p.~n", [username_validation(Username)]),
-    Req = cowboy_req:reply(200,
-        #{<<"content-type">> => <<"application/json">>},
-        Data,
-        Req1),
-    {ok, Req, State}.
+    io:format("~p.~n", [passwordConfirm_validation(PasswordConfirm)]),
+    UsernameValid = username_validation(Username),
+    if element(1,UsernameValid) == false -> 
+        Req = cowboy_req:reply(200,
+            #{<<"content-type">> => <<"application/json">>},
+            element(2, UsernameValid),
+            Req1),
+        {ok, Req, State};
+    true ->
+        Req = cowboy_req:reply(200,
+            #{<<"content-type">> => <<"application/json">>},
+            Data,
+            Req1),
+        {ok, Req, State}
+    end.
